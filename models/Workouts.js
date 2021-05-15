b8469c4ffb6c12a1e79cb9add5fb50f3de473279
Rayna W <raynagweinreb@gmail.com>
2021-05-15 12:45:54 -0700

    add workout model

---
+++ models/Workouts.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema (
+    {
+    type: {
+        type: String,
+        enum:["Cardio", "Weights","Resistance"],
+        required:"Please select Cardio, Weights or Resistance "
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: "Please enter your workout name"
+    },
+    duration: {
+        type: Number,
+        required: "Please enter the duration of your workout"
+    },
+    reps: {
+        type: Number,
+        
+    },
+    weight: {
+        type: Number,
+        
+    },
+    sets: {
+        type: Number,
+        
+    },
+    distance: {
+        type: Number,
+       
+    }
+
+    }
+)
+const Workout = mongoose.model("workout",workoutSchema)
+module.exports = Workout
